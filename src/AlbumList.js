7f5cb5e8bd054f450597244018c3c5ad4f97f893
Jason Francki <jasonfrancki@gmail.com>
2023-02-20 10:37:42 -0600

    refactor router

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -1,25 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import imageUrlBuilder from '@sanity/image-url'
 import './AlbumList.css'
-import { Link } from 'react-router-dom'
+import { Link, ScrollRestoration } from 'react-router-dom'
 
-const AlbumList = () => {
-  const [albums, setAlbums] = useState(null)
-  const [results, setResults] = useState(null)
-
-  const getAlbums = async () => {
-    const response = await fetch(
-      'https://8v3qbg1x.api.sanity.io/v2021-06-07/data/query/production?query=*[_type%20==%20%22album%22]'
-    )
-    const data = await response.json()
-    setAlbums(data.result)
-    setResults(data.result)
-  }
-
-  useEffect(() => {
-    getAlbums()
-  }, [])
-
+const AlbumList = ({ results }) => {
   const builder = imageUrlBuilder({
     projectId: '8v3qbg1x',
     dataset: 'production',
@@ -50,30 +34,29 @@ const AlbumList = () => {
           .map((album) => {
             const { artist, albumTitle, cover, altImg, deezerId } = album
             return (
-              <Link className='album' to={`/album/${deezerId}`}>
+              <Link key={album._id} className='album' to={`/album/${deezerId}`}>
                 <li
-                  id={artist[0]}
-                  key={album._id}
-                  onClick={(e) => {
-                    if (altImg) {
-                      if (
-                        urlFor(cover.asset._ref).width(500).url() ===
-                        e.currentTarget.children[0].src
-                      ) {
-                        e.currentTarget.children[0].src = urlFor(
-                          altImg.asset._ref
-                        )
-                          .width(500)
-                          .url()
-                      } else {
-                        e.currentTarget.children[0].src = urlFor(
-                          cover.asset._ref
-                        )
-                          .width(500)
-                          .url()
-                      }
-                    }
-                  }}
+                // id={artist[0]}
+                // onClick={(e) => {
+                //   if (altImg) {
+                //     if (
+                //       urlFor(cover.asset._ref).width(500).url() ===
+                //       e.currentTarget.children[0].src
+                //     ) {
+                //       e.currentTarget.children[0].src = urlFor(
+                //         altImg.asset._ref
+                //       )
+                //         .width(500)
+                //         .url()
+                //     } else {
+                //       e.currentTarget.children[0].src = urlFor(
+                //         cover.asset._ref
+                //       )
+                //         .width(500)
+                //         .url()
+                //     }
+                //   }
+                // }}
                 >
                   {cover ? (
                     <img
@@ -87,7 +70,7 @@ const AlbumList = () => {
 
                   <h3>{artist}</h3>
                   <h4>{albumTitle}</h4>
-                  {altImg ? <h3 className='more'>...</h3> : ''}
+                  {/* {altImg ? <h3 className='more'>...</h3> : ''} */}
                 </li>
               </Link>
             )
--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import AlbumList from './AlbumList'
 import Album from './Album'
 import './App.css'
 
 function App() {
+  const [albums, setAlbums] = useState(null)
+  const [results, setResults] = useState(null)
+
+  const getAlbums = async () => {
+    const response = await fetch(
+      'https://8v3qbg1x.api.sanity.io/v2021-06-07/data/query/production?query=*[_type%20==%20%22album%22]'
+    )
+    const data = await response.json()
+    setAlbums(data.result)
+    setResults(data.result)
+  }
+
+  useEffect(() => {
+    getAlbums()
+  }, [])
+
   return (
-    <Routes>
-      <Route path='/' element={<AlbumList />} />
-      <Route path='/album/:deezerId' element={<Album />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AlbumList results={results} />} />
+        <Route path='/album/:deezerId' element={<Album />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
